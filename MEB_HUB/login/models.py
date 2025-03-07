7c7e490031215792e0f06f13c5fa938e71ed8aea
Nhlamulo Mokwena <mokwenaphillemon9@gmail.com>
2025-03-07 08:11:37 +0200

    Setting up project and first commit

---
+++ MEB_HUB/login/models.py
@@ -0,0 +1,48 @@
+from msilib.schema import tables
+from tkinter.constants import CASCADE
+
+from django.db import models
+from django.db.models import IntegerField, CharField, OneToOneField, ForeignKey
+
+
+# Create your models here.
+
+class Campus(models.Model):
+    campus_id = models.IntegerField(primary_key=True)
+    campus_name = models.CharField(max_length=164)
+    location = models.CharField(max_length=164)
+
+    def __str__(self):
+        return f"{self.campus_id} {self.campus_name} {self.location}"
+
+class Student(models.Model):
+      studentNumber = models.CharField(max_length=20, primary_key=True)
+      name = models.CharField(max_length=100,default="")
+      surname = models.CharField(max_length=100,default="")
+      studentEmail = models.CharField(max_length=150)
+      password = models.CharField(max_length=150)
+      stud_card_image = models.ImageField(upload_to='images',null=True)
+      campus_id = models.ForeignKey(Campus,on_delete=models.CASCADE)
+
+      def __str__(self):
+          return f"{self.studentNumber} {self.name} {self.surname}"
+
+
+class RegisteredStudent(models.Model):
+    studentNumber = models.CharField(max_length=20,primary_key=True)
+    campus_id = models.ForeignKey(Campus,on_delete=models.CASCADE)
+
+    def __str__(self):
+       return f"{self.studentNumber} {self.campus_id}"
+
+class Admin(models.Model):
+    admin_id = IntegerField(primary_key=True)
+    name = CharField(max_length=100)
+    surname = CharField(max_length=100)
+    contact = CharField(max_length=10)
+    email = CharField(max_length=100)
+    password = CharField(max_length=100)
+    campus_id = ForeignKey(Campus,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.admin_id} {self.name} {self.surname}"
---
+++ MEB_HUB/login/views.py
@@ -0,0 +1,82 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.template import loader
+from django.contrib import messages
+
+from .models import Student, RegisteredStudent, Admin
+
+
+# Create your views here.
+def login(request):
+    if request.method == 'POST':
+
+        username = request.POST['email']
+        password = request.POST['password']
+
+        if Student.objects.filter(studentEmail=username).exists or Admin.objects.filter(email=username).exists():
+
+            if "@tut4life.ac.za" in username:
+
+                try:
+                    stud = Student.objects.all().get(studentEmail=username)
+                    if stud.password == password:
+                        return HttpResponse(f"Welcome {stud.name}")
+                    else:
+                        messages.error(request, "Inccorect Username / Password!")
+                        return redirect("/login")
+
+                except:
+                    messages.error(request, "Inccorect Username / Password!")
+                    return redirect("/login")
+
+            elif "@mebhub.ac.za" in username:
+                try:
+
+                    admin = Admin.objects.all().get(email=username)
+
+                    if admin.password == password:
+                        return HttpResponse(f"Welcome Administrator {admin.name}")
+                    else:
+                        messages.error(request, "Inccorect Username / Password!")
+                        return redirect("/login")
+                except:
+                    messages.error(request, "Incorrect Username / Password!")
+                    return redirect('/login')
+        else:
+            messages.error(request, 'Account does not exist!')
+            return redirect('/login')
+    else:
+        return render(request, "login/login.html")
+
+
+def register(request):
+    if request.method == 'POST':
+        student_no = request.POST['student_no']
+        name = request.POST['name']
+        surname = request.POST['surname']
+        student_email = request.POST['student_email']
+        password = request.POST['password']
+        password_confirmation = request.POST['confirm_password']
+
+        if RegisteredStudent.objects.filter(studentNumber=student_no).exists():
+            if password == password_confirmation:
+                camp_id = RegisteredStudent.objects.all().get(studentNumber=student_no).campus_id
+                student = Student(studentNumber=student_no, name=name, surname=surname,
+                                  studentEmail=student_email, password=password,
+                                  campus_id=camp_id)
+                student.save()
+                messages.success(request,"Account Created!")
+                return redirect("/login/register")
+            else:
+                messages.error(request,"password not matching")
+                return redirect('/login/register')
+        else:
+            messages.error(request,"student not registerd on tut")
+            return redirect('/login/register')
+
+    else:
+        return render(request, "login/register.html")
+
+
+def admin(request):
+    return render(request, "login/admin.html")
