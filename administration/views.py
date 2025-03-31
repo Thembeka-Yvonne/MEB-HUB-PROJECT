d9086ea0b3acef64e02b34617edafdb869c0adf2
Nhlamulo Mokwena <mokwenaphillemon9@gmail.com>
2025-03-31 17:23:52 +0200

    Creating crud operation for campus entity

--- administration/views.py
+++ administration/views.py
@@ -23,6 +23,56 @@ def add_campus(request):
     return HttpResponseRedirect(redirect_to='add_campus')
   else:
    return render(request,"admin/campus/add_campus.html")
+ 
+ 
+def list_all_campus(request):
+  campus_list = Campus.objects.all()
+  return render(request,"admin/campus/remove_campus.html",{
+    "campus_list": campus_list
+  })
+  
+def remove_campus(request,id):
+  campus = Campus.objects.all().get(campus_id=id)
+  campus.delete()
+  return HttpResponseRedirect(redirect_to='/administration/list_all_campus')
+  
+  
+def modify_list_campus(request):
+  campus_list = Campus.objects.all()
+  return render(request,"admin/campus/modify_campus.html",{
+    "campus_list" : campus_list
+  })
+  
+  
+def modify_campus(request,id):
+  
+  campus = Campus.objects.all().get(campus_id=id)
+  
+  if request.method == 'POST':
+    
+    camp_name = request.POST['name']
+    camp_loc = request.POST['location']
+    
+    campus.campus_name = camp_name
+    campus.location = camp_loc
+    
+    campus.save()
+    
+    return HttpResponseRedirect(redirect_to='/administration/modify_list_campus')
+  
+  else:
+    
+    return render(request,"admin/campus/modify_initial_campus.html",{
+      "campus": campus
+    })
+
+  
+def view_all_campuses(request):
+  list = Campus.objects.all()
+  return render(request,"admin/campus/view_all_campuses.html",{
+    "list": list
+  })
+
 
 def events_menu(request):
   return render(request,"admin/events/events_menu.html")
