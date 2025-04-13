dbea7e96f0e2da0a47e07b73014635d7cbd2863a
Thembeka-Yvonne <thembekayvonne140704@gmail.com>
2025-04-13 15:08:42 +0200

    These are the changes I made to the events

--- events/urls.py
+++ events/urls.py
@@ -3,8 +3,12 @@ from .import views
 from django.urls import path
 
 urlpatterns = [
-    path('add_event',views.add_event,name="add_event"),
+    path('add_event/', views.add_event, name="add_event"),
+    path('success', views.success, name="success"),
     path('update_events',views.update_events,name="update_events"),
     path('update_event_page',views.update_event_page,name="update_event_page"),
-    path('delete_event',views.delete_event,name="delete_event")
+    path('delete_events',views.delete_events,name="delete_events"),
+    path('delete_event_page/',views.delete_event_page,name="delete_event_page"),
+    path('events_home/', views.events_home, name="events_home")
+
 ]
