b90b6b145166814b6354b005e11c3324067c8401
Nhlamulo Mokwena <mokwenaphillemon9@gmail.com>
2025-04-28 15:34:14 +0200

    fixing bugs

--- administration/urls.py
+++ administration/urls.py
@@ -6,11 +6,13 @@ from . import views
 urlpatterns = [
     
     path('',views.admin_home,name="admin_home"),
+
     path('events_menu',views.events_menu, name="events_menu"),
     
     #url patterns for the bus
     
     path('bus_menu',views.bus_menu, name="bus_menu"),
+    path('add_all_campus_view',views.add_all_campuses_view,name="add_all_campus_view"),
     path('add_bus_schedule/<int:code>/',views.add_bus_schedule,name="add_bus_schedule"),
     
     #url pattern for the admin home
